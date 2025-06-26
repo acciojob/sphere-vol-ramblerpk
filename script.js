30049f569f96fab0d24a52fb539c99c0d20982c1
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-26 19:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault();
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    let radius = parseFloat(document.getElementById('radius').value);
+
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById('volume').value = 'NaN';
+        return;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4);
+    document.getElementById('volume').value = volume;
+}
+
+window.onload = function () {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+}
